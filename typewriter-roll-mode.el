53ebead9c230454d51b9532c839943b95b15a8f8
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2023-09-24 18:30:18 +0200

    [FIX] Package linting

--- typewriter-roll-mode.el
+++ typewriter-roll-mode.el
@@ -1,10 +1,11 @@
-;;; typewriter-roll-mode.el --- minor mode to force writing on first line only
+;;; typewriter-roll-mode.el --- Minor mode to force writing on first line only -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2023 Peter Badida
 
 ;; Author: Peter Badida <keyweeusr@gmail.com>
-;; Keywords: line, carriage, carriage return, writing, distraction, cr, rewind
+;; Keywords: convenience, line, carriage, writing, distraction, cr, rewind
 ;; Version: 1.0.0
+;; Homepage: https://github.com/KeyWeeUsr/typewriter-roll-mode
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -28,53 +29,55 @@
 
 ;;; Code:
 
-(defun twroll-scroll-up ()
-  "Scroll current line to the top"
+(defun typewriter-roll-scroll-up ()
+  "Scroll current line to the top."
   (inline)
   (recenter-top-bottom 0))
 
-(defun twroll-scroll-or-nothing (pos)
-  "Scroll if the cursor pos changed (content wrapped)"
+(defun typewriter-roll-scroll-or-nothing (pos)
+  "Scroll if the cursor POS changed (content wrapped)."
   (inline)
   (if (not (eq pos (current-column)))
-      (twroll-scroll-up)))
+      (typewriter-roll-scroll-up)))
 
-(defun twroll-scroll-main (pos)
-  "Main function for checking the first line scrolled to the top"
+(defun typewriter-roll-scroll-main (pos)
+  "Main function for checking the first line scrolled to the top.
+Argument POS cursor's position."
   (inline)
   (progn
     (fill-paragraph)
-    (twroll-scroll-or-nothing pos)))
+    (typewriter-roll-scroll-or-nothing pos)))
 
-(defun twroll-is-backspace ()
-  "Check if current command is a backspace (left delete)"
+(defun typewriter-roll-is-backspace ()
+  "Check if current command is a backspace (left delete)."
   (inline)
   (eq this-command 'delete-backward-char))
 
-(defun twroll-check ()
-  "Check after typing whether to scroll up as in typewriter"
+(defun typewriter-roll-check ()
+  "Check after typing whether to scroll up as in typewriter."
   (interactive)
-  (when (or (twroll-is-backspace) (eq last-command-event 32))
+  (when (or (typewriter-roll-is-backspace) (eq last-command-event 32))
     (if (not (eq (char-before) 32))
-        (twroll-scroll-main (current-column))
-      (twroll-scroll-up))))
+        (typewriter-roll-scroll-main (current-column))
+      (typewriter-roll-scroll-up))))
 
 (defun typewriter-roll-mode-activate ()
-  "Activate TypewriterRoll locally to a buffer"
+  "Activate TypewriterRoll locally to a buffer."
   (inline)
   ;; depth=nil, local to the current buffer=t
-  (add-hook 'post-self-insert-hook #'twroll-check nil t)
-  (add-hook 'pre-command-hook #'twroll-check nil t))
+  (add-hook 'post-self-insert-hook #'typewriter-roll-check nil t)
+  (add-hook 'pre-command-hook #'typewriter-roll-check nil t))
 
 (defun typewriter-roll-mode-deactivate ()
-  "Dectivate TypewriterRoll locally to a buffer"
+  "Dectivate TypewriterRoll locally to a buffer."
   (inline)
   ;; local to the current buffer=t
-  (remove-hook 'post-self-insert-hook #'twroll-check t)
-  (remove-hook 'pre-command-hook #'twroll-check t))
+  (remove-hook 'post-self-insert-hook #'typewriter-roll-check t)
+  (remove-hook 'pre-command-hook #'typewriter-roll-check t))
 
 (define-minor-mode typewriter-roll-mode
-  "Minor mode to justify and scroll up text and keep only single line in focus"
+  "Minor mode to justify and scroll up text and keep only single
+line in focus"
   :lighter " typewriter-roll"
   (if typewriter-roll-mode
       (typewriter-roll-mode-activate)

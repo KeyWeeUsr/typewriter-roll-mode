3114d05731517d40972e2ed896806b25bdc0d8c2
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2023-10-10 00:07:03 +0200

    [REF] Make funcs private, drop `(inline)`

--- typewriter-roll-mode.el
+++ typewriter-roll-mode.el
@@ -30,54 +30,51 @@
 
 ;;; Code:
 
-(defsubst typewriter-roll-scroll-up ()
+(defsubst typewriter-roll--scroll-up ()
   "Scroll current line to the top."
   (recenter-top-bottom 0))
 
-(defsubst typewriter-roll-scroll-or-nothing (pos)
+(defsubst typewriter-roll--scroll-or-nothing (pos)
   "Scroll if the cursor POS changed (content wrapped)."
   (unless (eq pos (current-column))
-      (typewriter-roll-scroll-up)))
+      (typewriter-roll--scroll-up)))
 
-(defsubst typewriter-roll-scroll-main (pos)
+(defsubst typewriter-roll--scroll-main (pos)
   "Main function for checking the first line scrolled to the top.
 Argument POS cursor's position."
   (progn
     (fill-paragraph)
-    (typewriter-roll-scroll-or-nothing pos)))
+    (typewriter-roll--scroll-or-nothing pos)))
 
-(defsubst typewriter-roll-is-backspace ()
+(defsubst typewriter-roll--is-backspace ()
   "Check if current command is a backspace (left delete)."
   (eq this-command 'delete-backward-char))
 
-(defun typewriter-roll-check ()
+(defun typewriter-roll--check ()
   "Check after typing whether to scroll up as in typewriter."
-  (when (or (typewriter-roll-is-backspace) (eq last-command-event 32))
+  (when (or (typewriter-roll--is-backspace) (eq last-command-event 32))
     (unless (eq (char-before) 32)
-        (typewriter-roll-scroll-main (current-column))
-      (typewriter-roll-scroll-up))))
+        (typewriter-roll--scroll-main (current-column))
+      (typewriter-roll--scroll-up))))
 
-(defun typewriter-roll-mode-activate ()
+(defun typewriter-roll-mode--activate ()
   "Activate TypewriterRoll locally to a buffer."
-  (inline)
   ;; depth=nil, local to the current buffer=t
-  (add-hook 'post-self-insert-hook #'typewriter-roll-check nil t)
-  (add-hook 'pre-command-hook #'typewriter-roll-check nil t))
+  (add-hook 'post-self-insert-hook #'typewriter-roll--check nil t)
+  (add-hook 'pre-command-hook #'typewriter-roll--check nil t))
 
-(defun typewriter-roll-mode-deactivate ()
+(defun typewriter-roll-mode--deactivate ()
   "Dectivate TypewriterRoll locally to a buffer."
-  (inline)
   ;; local to the current buffer=t
-  (remove-hook 'post-self-insert-hook #'typewriter-roll-check t)
-  (remove-hook 'pre-command-hook #'typewriter-roll-check t))
+  (remove-hook 'post-self-insert-hook #'typewriter-roll--check t)
+  (remove-hook 'pre-command-hook #'typewriter-roll--check t))
 
 (define-minor-mode typewriter-roll-mode
-  "Minor mode to justify and scroll up text and keep only single
-line in focus"
+  "Justify and scroll text to keep a single line in focus."
   :lighter " typewriter-roll"
   (if typewriter-roll-mode
-      (typewriter-roll-mode-activate)
-    (typewriter-roll-mode-deactivate)))
+      (typewriter-roll-mode--activate)
+    (typewriter-roll-mode--deactivate)))
 
 (provide 'typewriter-roll-mode)
 ;;; typewriter-roll-mode.el ends here
